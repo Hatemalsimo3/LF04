0f08cb928513767bd6c714d6584dacd9991a3524
hatem3 <hatem3@schule.bremen.de>
2022-06-08 09:29:08 +0200

    Kommentar in Programm-Klasse

--- Kaufvertrag/src/dao/testDao.java
+++ Kaufvertrag/src/dao/testDao.java
@@ -11,28 +11,37 @@ import java.util.ArrayList;
 public class testDao {
     public static void main(String[] args) throws Exception {
 
-        VertragspartnerDao vertragspartnerDao=new VertragspartnerDao();               //.........................................Read
+        //.........................................Read
+        VertragspartnerDao vertragspartnerDao=new VertragspartnerDao();
         ArrayList<Vertragspartner>vertragspartnerArrayList= vertragspartnerDao.read();
         for (Vertragspartner v : vertragspartnerArrayList) {
             System.out.println(v);
         }
-
         //.....................................................................................................  Creat
       Vertragspartner vertragspartner1=new Vertragspartner("Hatem","Alsimo");
       vertragspartner1.setAdresse(new Adresse("Luneplate","12345","12112","Bremen"));
 
-      vertragspartnerDao.Delet("Hatem3");
-      //....................................................................................................... Delet
+       //---------------------------------------------------------------------------------------------------------------Delet
+          vertragspartnerDao.Delet("Hatem3");
+      //..........................................................Update
+        vertragspartnerDao.update(vertragspartner1);
 
-      //..................................................................................................... ware
-
+      //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::Ware
+        // _____________________________________________Read
       Waredao waredao=new Waredao();
       Ware ware=waredao.read(1);
       System.out.println(ware);
+
+      //_______________________________________________________Creat
+
       Ware ware1=new Ware(123,"Auto",1);
       ware.setBeschreibung(" Kann gut fahren");
+      //......................................................................................Delet
+        waredao.Delet(1);
 
-      //......................................................................................
+
+        //.............................................Update
+        waredao.update(ware1);
 
 
 
